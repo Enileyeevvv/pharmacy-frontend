477187260214a6eca36b2743a00a1990283cfdac
it108 <@bwg.it854>
2024-05-06 23:05:48 +0300

    1000-7

--- src/widgets/layout/ui/layout.tsx
+++ src/widgets/layout/ui/layout.tsx
@@ -3,7 +3,9 @@
 import { FC, PropsWithChildren } from "react"
 import { AppShell } from "@mantine/core"
 import { useDisclosure } from "@mantine/hooks"
+
 import type { LinkType } from "@/shared/types/link"
+
 import { Header } from "./header"
 import { Sidebar } from "./sidebar"
 
